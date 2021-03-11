40f09e2d1d261c0baaf1233f97c44627cc508cfb
AS81 <sanloginchik@gmail.com>
2021-03-12 02:04:12 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        // return Math.min.apply(null, array); // dont't work...
+        return array.sort((a, b) => a - b)[0];
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return array.sort((a, b) => a - b)[array.length - 1];
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return (
+            array.reduce((summ, current) => {
+                return current + summ;
+            }, 0) / array.length
+        );
+    }
+};
